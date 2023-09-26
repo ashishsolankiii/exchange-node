1c022bfd5094ab02a6706a77aa34cb06402eaa1f
pikachu <sumeet@rethinksoft.com>
2023-09-26 18:14:41 +0530

    config updates

---
+++ config/encryption.js
@@ -0,0 +1,10 @@
+export const encryptionConfig = {
+  fragmentLength: 100,
+  defragmentStep: 3,
+
+  responseKey: "cpr_ctx",
+
+  handshakeFragKey: "rel_buf1",
+  handshakeDefragKey: "rel_buf2",
+  handshakeDevEnvKey: "rel_buf3",
+};
