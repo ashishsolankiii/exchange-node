9ef9c61ad148faa0b92b558e3ae2421bc33c378b
pikachu <sumeet@rethinksoft.com>
2023-10-05 17:33:37 +0530

    cleanup

--- socket/namespaces/user-bet/userBetNamespace.js
+++ socket/namespaces/user-bet/userBetNamespace.js
@@ -4,7 +4,6 @@ import { validateAuth, validateUser } from "../../middlewares/userMiddleware.js"
 async function handleConnection(socket) {
   try {
     socket.on("event:bet", async ({ eventId }, callback) => {
-      console.log(socket.userId);
       if (!eventId) throw new Error("eventId is required.");
       const bets = await betService.fetchUserEventBets({ eventId, userId: socket.userId });
       callback(bets);
