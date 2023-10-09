43781c13eaec01d65edb26da02eb8443114e22d5
nihang-prog <nihang.rethinksoft@gmail.com>
2023-10-09 16:32:05 +0530

    remove confirm password

--- requests/v1/userRequest.js
+++ requests/v1/userRequest.js
@@ -255,10 +255,7 @@ async function changePasswordRequest(req) {
   const validationSchema = Yup.object().shape({
     loginUserId: Yup.string().required().test("userId", "Given _id is not valid!", isValidObjectId),
     oldPassword: Yup.string().required(),
-    newPassword: Yup.string().required(),
-    confirmPassword: Yup.string()
-      .oneOf([Yup.ref("newPassword")], "Passwords must match")
-      .required(),
+    newPassword: Yup.string().required()
   });
 
   await validationSchema.validate(req.body);
