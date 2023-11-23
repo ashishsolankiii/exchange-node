4041c80338f47cf96202e1e0653bbed167f9e118
Nihang <nihang.rethinksoft@gmail.com>
2023-11-23 12:51:11 +0530

    Add condition for external api

--- middlewares/encryptionMiddleware.js
+++ middlewares/encryptionMiddleware.js
@@ -10,21 +10,25 @@ import { decryptRequest, encryptResponse } from "../lib/io-guards/encryption.js"
  */
 export default function encryptResponseInterceptor(req, res, next) {
   try {
-    const isBypassed = req.get(encryptionConfig.encBypassHeader) === "true";
-    const isBypassKeyValid = req.get(encryptionConfig.encBypassKeyHeader) === appConfig.RESPONSE_AES_BYPASS_KEY;
+    // Split path for external api
+    const breakPath = req.path.split("/");
+    const findExternal = breakPath.filter((item) => item == 'external');
+    if (findExternal.length == 0) {
+      const isBypassed = req.get(encryptionConfig.encBypassHeader) === "true";
+      const isBypassKeyValid = req.get(encryptionConfig.encBypassKeyHeader) === appConfig.RESPONSE_AES_BYPASS_KEY;
 
-    // Encrypt response
-    const originalSend = res.json;
-    res.json = function (data) {
-      const responsePayload = isBypassed && isBypassKeyValid ? data : encryptResponse(data);
-      originalSend.call(this, responsePayload);
-    };
+      // Encrypt response
+      const originalSend = res.json;
+      res.json = function (data) {
+        const responsePayload = isBypassed && isBypassKeyValid ? data : encryptResponse(data);
+        originalSend.call(this, responsePayload);
+      };
 
-    // Decrypt request
-    if (encryptionConfig.requestKey in req.body) {
-      req.body = decryptRequest(req.body[encryptionConfig.requestKey]);
+      // Decrypt request
+      if (encryptionConfig.requestKey in req.body) {
+        req.body = decryptRequest(req.body[encryptionConfig.requestKey]);
+      }
     }
-
     next();
   } catch (error) {
     const encryptedError = encryptResponse(error.message);
