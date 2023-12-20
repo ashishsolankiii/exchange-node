380a8f3790c75c171febe0f7f1998031ee720989
pikachu <sumeet@rethinksoft.com>
2023-12-20 17:54:54 +0530

    Add jsdoc to app config

--- config/app.js
+++ config/app.js
@@ -9,6 +9,34 @@ const { env } = process;
  *
  * Add all the .env variables to this object.
  * This will help with autocomplete suggestions.
+ *
+ * @type {object}
+ *
+ * @property {string} NODE_ENV - The application environment.
+ * @property {number} PORT - The application port.
+ * @property {string} DEV_USER - The development user.
+ * @property {string} ADMIN_CLIENT_URL - The admin client URL.
+ * @property {string} USER_CLIENT_URL - The user client URL.
+ * @property {string} THEME_PANEL_CLIENT_URL - The theme panel client URL.
+ * @property {string} TRANSACTION_PANEL_CLIENT_URL - The transaction panel client URL.
+ * @property {string} CORS_ALLOWED_ORIGINS - The CORS allowed origins.
+ * @property {number} SALT_ROUNDS - The salt rounds for password hashing.
+ * @property {string} JWT_TOKEN_PREFIX - The JWT token prefix.
+ * @property {string} JWT_SECRET - The JWT secret.
+ * @property {string} JWT_EXPIRY - The JWT expiry.
+ * @property {string} TRANSACTION_AES_SECRET - The transaction AES secret.
+ * @property {string} PERMISSIONS_AES_SECRET - The permissions AES secret.
+ * @property {string} RESPONSE_AES_SECRET - The response AES secret.
+ * @property {string} RESPONSE_AES_BYPASS_KEY - The response AES bypass key.
+ * @property {string} MONGO_URL - The MongoDB URL.
+ * @property {string} BASE_URL - The base URL.
+ * @property {string} AWS_S3_BUCKET - The AWS S3 bucket.
+ * @property {object} AWS_S3_CONFIG - The AWS S3 config.
+ * @property {boolean} LOG_ENABLED - The log enabled.
+ * @property {string} LOG_PATH - The log path.
+ * @property {number} LOG_MAX_FILE_SIZE - The log max file size.
+ * @property {number} LOG_MAX_HISTORY - The log max history.
+ * @property {string} LOG_DATE_FORMAT - The log date format.
  */
 export const appConfig = {
   NODE_ENV: env.NODE_ENV,
