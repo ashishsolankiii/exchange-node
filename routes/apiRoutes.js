552b46af851850347defc8d4a00010f56885d9d0
pikachu <sumeet@rethinksoft.com>
2023-12-22 11:46:15 +0530

    Create route for aura callback

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
+import auraRoutes from "./auraRoutes.js";
 import apiRoutesV1 from "./v1/apiRoutesV1.js";
 
 const app = express();
 
 app.use("/v1", apiRoutesV1);
+app.use("/poker", auraRoutes);
 
 export default app;
