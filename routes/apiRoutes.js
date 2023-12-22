f94976587b6bf861481a5673c3e73a557dc827e2
pikachu <sumeet@rethinksoft.com>
2023-12-22 11:48:46 +0530

    Add v1 and Aura Casino routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,7 +4,10 @@ import apiRoutesV1 from "./v1/apiRoutesV1.js";
 
 const app = express();
 
+// Application v1 routes
 app.use("/v1", apiRoutesV1);
+
+// Aura Casino callback routes
 app.use("/poker", auraRoutes);
 
 export default app;
