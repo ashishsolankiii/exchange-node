ec2d7784a27332c746cb9692a1816a5a07d57366
pikachu <sumeet@rethinksoft.com>
2023-12-29 13:12:28 +0530

    Create flow to generate aura launch url

--- routes/auraRoutes.js
+++ routes/auraRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import auraCasinoController from "../controllers/v1/auraCasinoController.js";
-import { authenticateOperator, authenticateToken } from "../middlewares/auraCasinoMiddleware.js"
+import { authenticateOperator, authenticateToken } from "../middlewares/auraCasinoMiddleware.js";
+import passportJwtMiddleware from "../middlewares/passportJwtMiddleware.js";
 const router = express.Router();
 
 router.post("/auth", authenticateOperator, authenticateToken, auraCasinoController.getUserData);
 router.post("/exposure", auraCasinoController.getExposure);
 router.post("/results", auraCasinoController.getResult);
+router.post("/getLaunchUrl", passportJwtMiddleware, auraCasinoController.getLaunchUrl);
 
 export default router;
