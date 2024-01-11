b42607ad99800f2c2ffdaa11f0165262cbc2e390
Nihang <nihang.rethinksoft@gmail.com>
2024-01-11 10:46:17 +0530

    Changes as commented

--- routes/worldCasinoRotes.js
+++ routes/worldCasinoRotes.js
@@ -2,8 +2,8 @@ import express from "express";
 import worldCasinoController from "../controllers/v1/worldCasinoController.js";
 const router = express.Router();
 
-router.post("/balance", worldCasinoController.balance);
-router.post("/credit", worldCasinoController.credit);
+router.post("/Balance", worldCasinoController.balance);
+router.post("/Credit", worldCasinoController.credit);
 router.post("/debit", worldCasinoController.debit);
 
 export default router;
