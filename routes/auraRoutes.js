6376ca2c8e915044dfaecbbb6e37d75ff2928326
pikachu <sumeet@rethinksoft.com>
2024-01-11 17:55:47 +0530

    encryption updates

--- routes/auraRoutes.js
+++ routes/auraRoutes.js
@@ -1,12 +1,13 @@
 import express from "express";
 import auraCasinoController from "../controllers/v1/auraCasinoController.js";
 import { authenticateOperator, authenticateToken } from "../middlewares/auraCasinoMiddleware.js";
+import encryptResponseInterceptor from "../middlewares/encryptionMiddleware.js";
 import passportJwtMiddleware from "../middlewares/passportJwtMiddleware.js";
 const router = express.Router();
 
 router.post("/auth", authenticateOperator, authenticateToken, auraCasinoController.getUserData);
 router.post("/exposure", auraCasinoController.getExposure);
 router.post("/results", auraCasinoController.getResult);
-router.post("/getLaunchUrl", passportJwtMiddleware, auraCasinoController.getLaunchUrl);
+router.post("/getLaunchUrl", [encryptResponseInterceptor, passportJwtMiddleware], auraCasinoController.getLaunchUrl);
 
 export default router;
