7ba74d8186faa3a64ddd657323325fb2503b103f
Nihang <nihang.rethinksoft@gmail.com>
2024-01-12 13:56:21 +0530

    Add authentication world casino

--- routes/worldCasinoRotes.js
+++ routes/worldCasinoRotes.js
@@ -1,9 +1,11 @@
 import express from "express";
 import worldCasinoController from "../controllers/v1/worldCasinoController.js";
 const router = express.Router();
+import { route } from "../lib/error-handling/routes-error-boundary.js";
 
 router.post("/Balance", worldCasinoController.balance);
 router.post("/Credit", worldCasinoController.credit);
 router.post("/debit", worldCasinoController.debit);
+route(router, "post", "/getLaunchUrl", worldCasinoController.getLaunchUrl);
 
 export default router;
